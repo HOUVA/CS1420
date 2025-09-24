ba1e05e0c314de4c54e0e373fc214ccfd2c93e6f
Matthew Suggars <matt.suggars@gmail.com>
2025-09-24 16:53:06 -0600

    Final changes to project

--- src/assign05/CalendarDateTest.java
+++ src/assign05/CalendarDateTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 /**
  * This class contains unit tests to check the correctness of the CalendarDate class.
  * 
@@ -11,10 +13,13 @@ import org.junit.jupiter.api.Test;
  * @version September 23, 2025.
  */
 public class CalendarDateTest {
+		
+	/*
+	 * Test Cases for Constructors
+	 */
 	@Test
 	public void testNoParameterConstructor() {
-		CalendarDate date = new CalendarDate();
-		
+		CalendarDate date = new CalendarDate();		
 		assertEquals(1, date.getDay(), "No-parameter constructor does not set day number to 1");
 		assertEquals(1, date.getMonth().getMonthNumber(), "No-parameter constructor does not set month number to 1");
 		assertEquals(1000, date.getYear(), "No-parameter constructor does not set year number to 1000");
@@ -23,11 +28,14 @@ public class CalendarDateTest {
 	@Test
 	public void finalExamConstructor() {
 		CalendarDate date = new CalendarDate(12, 8, 2025);
-		assertEquals(8, date.getDay(), "constructor does not set day number to 8");
-		assertEquals(12, date.getMonth().getMonthNumber(), "No-parameter constructor does not set month number to 12");
-		assertEquals(2025, date.getYear(), "No-parameter constructor does not set year number to 2025");
+		assertEquals(8, date.getDay(), "Constructor does not set day number to 8");
+		assertEquals(12, date.getMonth().getMonthNumber(), "Constructor does not set month number to 12");
+		assertEquals(2025, date.getYear(), "Constructor does not set year number to 2025");
 	}
 	
+	/*
+	 * Test cases for getDay, getYear, and getMonth
+	 */
 	@Test
 	public void testGetDay() {
 		CalendarDate date = new CalendarDate(3, 15, 1950);
@@ -46,24 +54,75 @@ public class CalendarDateTest {
 		assertEquals(3, date.getMonth().getMonthNumber(), "getMonth method is incorrect");
 	}
 	
+	/*
+	 * Test cases for toString
+	 */
 	@Test
-	public void testToString() {
+	public void testToStringNormalCase() {
 		CalendarDate date = new CalendarDate(8, 1, 1970);
 		assertEquals("August 1, 1970", date.toString(),
 				"toString does not return correct String -- check for typos");
 	}
 	
 	@Test
-	public void testComesBeforeTrue() {
+	public void testToStringDefaultConstructor() {
+		CalendarDate date = new CalendarDate();
+		assertEquals("January 1, 1000", date.toString(),
+				"toString does not return correct String -- check for typos");
+	}
+	
+	@Test
+	public void testToStringBirthday() {
+		CalendarDate date = new CalendarDate(6, 9, 1999);
+		assertEquals("June 9, 1999", date.toString(),
+				"toString does not return correct String -- check for typos");
+	}
+	
+	/*
+	 * Test cases for comesBefore
+	 */
+	@Test
+	public void testComesBeforeDifferentMonthTrue() {
 		CalendarDate date = new CalendarDate(5, 19, 1985);
 		assertTrue(date.comesBefore(new CalendarDate(6, 9, 1985)),
 				"comesBefore does not return true when date on which method is called comes before argument");
 	}
 	
-	public void testComesBeforeFalse() {
+	@Test
+	public void testComesBeforeDifferentMonthFalse() {
+		CalendarDate date = new CalendarDate(5, 19, 1985);
+		assertFalse(date.comesBefore(new CalendarDate(4, 9, 1985)),
+				"comesBefore does not return false when date on which method is called comes before argument");
+	}
+	
+	@Test
+	public void testComesBeforeDifferentDayTrue() {
+		CalendarDate date = new CalendarDate(5, 19,1985);
+		assertTrue(date.comesBefore(new CalendarDate(5, 20, 1985)),
+				"comesBefore does not return true when date on which method is called comes after argument");
+	}
+	
+	@Test 
+	public void testComesBeforeDifferentDayFalse() {
 		CalendarDate date = new CalendarDate(5, 19,1985);
 		assertFalse(date.comesBefore(new CalendarDate(5, 18, 1985)),
 				"comesBefore does not return false when date on which method is called comes after argument");
+	}
+	
+	@Test
+	public void testComesBeforeDifferentYearTrue() {
+		CalendarDate date = new CalendarDate(5, 19,1985);
+		CalendarDate otherDate = new CalendarDate(5, 19,1986);
+		assertTrue(date.comesBefore(otherDate),
+				"comesBefore does not return true when date on which method is called comes before argument");
+	}
+	
+	@Test
+	public void testComesBeforeDifferentYearFalse() {
+		CalendarDate date = new CalendarDate(5, 19,1985);
+		CalendarDate otherDate = new CalendarDate(5, 19,1984);
+		assertFalse(date.comesBefore(otherDate),
+				"comesBefore does not return false when date on which method is called comes before argument");
 	}
 	
 	@Test
@@ -73,6 +132,9 @@ public class CalendarDateTest {
 				"comesBefore does not return false when dates are same");
 	}
 	
+	/*
+	 * Test cases for comesAfter
+	 */
 	@Test
 	public void testComesAfterSameDate() {
 		CalendarDate date = new CalendarDate(2, 7, 1888);
@@ -81,12 +143,50 @@ public class CalendarDateTest {
 	}
 	
 	@Test
-	public void testComesAfterTrue() {
+	public void testComesAfterDifferentDayTrue() {
 		CalendarDate date = new CalendarDate(2, 7, 1888);
-		assertTrue(date.comesAfter(new CalendarDate(1, 7, 1888)),
-				"comesAfter does not return false when date on which method is called is the same as argument");
+		assertTrue(date.comesAfter(new CalendarDate(2, 6, 1888)),
+				"comesAfter does not return true when date on which method is called comes after argument");
 	}
 	
+	@Test
+	public void testComesAfterDifferentDayFalse() {
+		CalendarDate date = new CalendarDate(2, 7, 1888);
+		assertFalse(date.comesAfter(new CalendarDate(2, 8, 1888)),
+				"comesAfter does not return false when date on which method is called does not come after argument");
+	}
+	
+	@Test
+	public void testComesAfterDifferentMonthTrue() {
+		CalendarDate date = new CalendarDate(2, 7, 1888);
+		assertTrue(date.comesAfter(new CalendarDate(1, 7, 1888)),
+				"comesAfter does not return true when date on which method is called comes after argument");
+	}
+	
+	@Test
+	public void testComesAfterDifferentMonthFalse() {
+		CalendarDate date = new CalendarDate(2, 7, 1888);
+		assertFalse(date.comesAfter(new CalendarDate(3, 7, 1888)),
+				"comesAfter does not return false when date on which method is called does not come after argument");
+	}
+	
+	@Test
+	public void testComesAfterDifferentYearTrue() {
+		CalendarDate date = new CalendarDate(2, 7, 1888);
+		assertTrue(date.comesAfter(new CalendarDate(2, 7, 1887)),
+				"comesAfter does not return true when date on which method is called comes after argument");
+	}
+	
+	@Test
+	public void testComesAfterDifferentYearFalse() {
+		CalendarDate date = new CalendarDate(2, 7, 1888);
+		assertFalse(date.comesAfter(new CalendarDate(3, 7, 1889)),
+				"comesAfter does not return false when date on which method is called does not come after argument");
+	}
+	
+	/*
+	 * Test cases for advanceOneDay
+	 */
 	@Test
 	public void testAdvanceOneDay() {
 		CalendarDate date = new CalendarDate(3, 29, 1200);
@@ -94,7 +194,7 @@ public class CalendarDateTest {
 		assertEquals(3, date.getMonth().getMonthNumber(),
 				"advanceOneDay changed month when not at end of month");
 		assertEquals(30, date.getDay(),
-				"advanceOneDay does not add 1 to 1 when not add end ");
+				"advanceOneDay does not add 1 to day when not add end ");
 		assertEquals(1200, date.getYear(),
 				"advanceOneDay changed year when at the end of the month (not December)");
 	}
@@ -123,6 +223,9 @@ public class CalendarDateTest {
 				"advanceOneDay does not add 1 to year when at the end of the year");
 	}
 	
+	/*
+	 * Test Cases for DayOfYear
+	 */
 	@Test
 	public void testDayOfYearFirst() {
 		CalendarDate date = new CalendarDate(1, 1, 3000);
@@ -130,15 +233,91 @@ public class CalendarDateTest {
 	}
 	
 	@Test
+	public void testDayOfYearNormalCase() {
+		CalendarDate date = new CalendarDate(1, 2, 3000);
+		assertEquals(2, date.dayOfYear(), "dayOfYear does not return 2 for the January 2nd");
+	}
+	
+	@Test
+	public void testDayOfYearLastDayOfYear() {
+		CalendarDate date = new CalendarDate(12, 31, 3000);
+		assertEquals(365, date.dayOfYear(), "dayOfYear does not return 365 for the first day of a year");
+	}
+	
+	@Test
+	public void testDayOfYearLastDayOfYearLeapYear() {
+		CalendarDate date = new CalendarDate(12, 31, 2000);
+		assertEquals(366, date.dayOfYear(), "dayOfYear does not return 366 for the first day of a leap year");
+	}
+	
+	@Test
+	public void testDayOfYearBirthday() {
+		CalendarDate date = new CalendarDate(6, 9, 1999);
+		assertEquals(160, date.dayOfYear(), "dayOfYear does not return 160 for the birthday of a year");
+	}
+	
+	@Test
+	public void testDayOfYearBirthdayLeapYear() {
+		CalendarDate date = new CalendarDate(6, 9, 2000);
+		assertEquals(161, date.dayOfYear(), "dayOfYear does not return 161 for the birthday of a leap year");
+	}
+	
+	/*
+	 * Test cases for isLeapYear
+	 * TODO test more cases
+	 */
+	@Test
 	public void testIsLeapYearTrue() {
 		CalendarDate date = new CalendarDate(1, 1, 2004);
 		assertTrue(date.isLeapYear(), "isLeapYear does not return true for year divisible by 4 but not 100");
 	}
 	
 	@Test
-	public void testEqualsTrue() {
+	public void testIsLeapYearFalse() {
+		CalendarDate date = new CalendarDate(1, 1, 2005);
+		assertFalse(date.isLeapYear(), "isLeapYear does not return false for year not divisible by 4");
+	}
+	
+	@Test
+	public void testIsLeapYearTrueTwoThousand() {
+		CalendarDate date = new CalendarDate(1, 1, 2000);
+		assertTrue(date.isLeapYear(), "isLeapYear does not return true for year divisible by 4 and 100 and 400");
+	}
+	
+	@Test
+	public void testIsLeapYearFalseEighteenHundree() {
+		CalendarDate date = new CalendarDate(1, 1, 1800);
+		assertFalse(date.isLeapYear(), "isLeapYear does not return true for year divisible by 4 and 100 but not 400");
+	}
+	
+	/*
+	 * Test cases for equals
+	 */
+	@Test
+	public void testEqualsTrueSameDate() {
 		CalendarDate date = new CalendarDate(10, 10, 3333);
 		assertTrue(date.equals(new CalendarDate(10, 10, 3333)),
 				"equals method does not return true for same dates");
+	}
+	
+	@Test
+	public void testEqualsFalseDifferentDates() {
+		CalendarDate date = new CalendarDate(10, 10, 3333);
+		assertFalse(date.equals(new CalendarDate(10, 10, 3334)),
+				"equals method does not return false for different dates");
+	}
+	
+	@Test
+	public void testEqualsFalseDifferentObjectMonth() {
+		CalendarDate date = new CalendarDate(10, 10, 3333);
+		assertFalse(date.equals(new Month(3)),
+				"equals method does not return false for different Object type");
+	}
+	
+	@Test
+	public void testEqualsFalseDifferentObjectScanner() {
+		CalendarDate date = new CalendarDate(10, 10, 3333);
+		assertFalse(date.equals(new Scanner(System.in)),
+				"equals method does not return false for different Object type");
 	}
 }
--- src/assign05/MonthTest.java
+++ src/assign05/MonthTest.java
@@ -13,6 +13,10 @@ import org.junit.jupiter.api.Test;
  * @version September 23, 2025.
  */
 public class MonthTest {
+	
+	/*
+	 * Test cases for constructors
+	 */
 	@Test
 	public void testNoParameterConstructor() {
 		Month jan = new Month();
@@ -25,11 +29,94 @@ public class MonthTest {
 		assertEquals(3, mar.getMonthNumber(), "Constructor does not set month number to 3");
 	}
 	
+	/*
+	 * Test cases for toString for each month
+	 * 
+	 */
+	@Test
+	public void testToStringEmpty() {
+		Month none = new Month(0);
+		assertEquals(null, none.toString(), "toString does not return correct String -- check for typos");
+
+	}
 	
 	@Test
-	public void testToString() {
+	public void testToStringInvalidMonth() {
+		Month none = new Month(13);
+		assertEquals(null, none.toString(), "toString does not return correct String -- check for typos");
+
+	}
+	
+	@Test
+	public void testToStringJanuary() {
+		Month jan = new Month(1);
+		assertEquals("January", jan.toString(), "toString does not return correct String -- check for typos");
+	}
+	
+	@Test
+	public void testToStringFebruary() {
+		Month feb = new Month(2);
+		assertEquals("February", feb.toString(), "toString does not return correct String -- check for typos");
+	}
+	
+	@Test
+	public void testToStringMarch() {
+		Month mar = new Month(3);
+		assertEquals("March", mar.toString(), "toString does not return correct String -- check for typos");
+	}
+	
+	@Test
+	public void testToStringApril() {
+		Month apr = new Month(4);
+		assertEquals("April", apr.toString(), "toString does not return correct String -- check for typos");
+	}
+	
+	@Test
+	public void testToStringMay() {
+		Month may = new Month(5);
+		assertEquals("May", may.toString(), "toString does not return correct String -- check for typos");
+	}
+	
+	@Test
+	public void testToStringJune() {
+		Month jun = new Month(6);
+		assertEquals("June", jun.toString(), "toString does not return correct String -- check for typos");
+	}
+	
+	@Test
+	public void testToStringJuly() {
+		Month jul = new Month(7);
+		assertEquals("July", jul.toString(), "toString does not return correct String -- check for typos");
+	}
+	
+	@Test
+	public void testToStringAugust() {
+		Month aug = new Month(8);
+		assertEquals("August", aug.toString(), "toString does not return correct String -- check for typos");
+	}
+	
+	@Test
+	public void testToStringSeptember() {
+		Month sep = new Month(9);
+		assertEquals("September", sep.toString(), "toString does not return correct String -- check for typos");
+	}
+	
+	@Test
+	public void testToStringOctober() {
 		Month oct = new Month(10);
 		assertEquals("October", oct.toString(), "toString does not return correct String -- check for typos");
+	}
+	
+	@Test
+	public void testToStringNovember() {
+		Month nov = new Month(11);
+		assertEquals("November", nov.toString(), "toString does not return correct String -- check for typos");
+	}
+	
+	@Test
+	public void testToStringDecember() {
+		Month dec = new Month(12);
+		assertEquals("December", dec.toString(), "toString does not return correct String -- check for typos");
 	}
 	
 	@Test
@@ -44,6 +131,9 @@ public class MonthTest {
 		assertEquals(29, feb.lastDay(true), "Last day of April is incorrect");
 	}
 	
+	/*
+	 * Test cases for lastDay
+	 */
 	@Test
 	public void testLastDayFebruaryNotLeapYear() {
 		Month feb = new Month(2);
@@ -69,8 +159,8 @@ public class MonthTest {
 	
 	@Test
 	public void testLastDayJune() {
-		Month jun = new Month(2);
-		assertEquals(30, jun.lastDay(false), "Last day of April is incorrect");
+		Month jun = new Month(6);
+		assertEquals(30, jun.lastDay(false), "Last day of June is incorrect");
 	}
 	
 	@Test
@@ -109,8 +199,11 @@ public class MonthTest {
 		assertEquals(31, dec.lastDay(false), "Last day of May is incorrect");
 	}
 	
+	/*
+	 * Test cases for validDay
+	 */
 	@Test
-	public void testValidDayNormal() {
+	public void testValidDayJune() {
 		Month jun = new Month(6);
 		assertTrue(jun.validDay(10, false), "validDay incorrect for valid day");
 	}
@@ -127,6 +220,9 @@ public class MonthTest {
 		assertFalse(feb.validDay(29, false), "validDay incorrect for normal year February");
 	}
 	
+	/*
+	 * Test cases for equals
+	 */
 	@Test
 	public void testEqualsFalse() {
 		Month sep = new Month(9);
