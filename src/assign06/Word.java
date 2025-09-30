8a85becccfec261d85029ac1f5cbe9a3fd2d37c5
Matthew Suggars <42852733+HOUVA@users.noreply.github.com>
2025-09-30 00:08:53 -0600

    minor changes

--- src/assign06/Word.java
+++ src/assign06/Word.java
@@ -39,12 +39,28 @@ public class Word {
 	   return letters[startIndex] + toString(startIndex + 1);
 	}
 	
+	/**
+	 * Returns the number of times the letter passed appears in the Word object.
+	 * (driver method).
+	 * 
+	 * @param letter - must be between A-Z or a-z.
+	 * @return the number of occurences of letter in Word.
+	 */
 	public int countOccurrences(char letter) {
 		if (!((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z')))
 			throw new IllegalArgumentException("argument passed is not a letter");
 		return countOccurrences(letter, 0);
 	}
 	
+	
+	/**
+	 * Returns an integer representing the number of occurrences of the letter in the Word.
+	 * (recursive method).
+	 * 
+	 * @param letter - must be between A-Z or a-z
+	 * @param index the index of letters array to check the occurrence
+	 * @return the number of occurrences in the entire array.
+	 */
 	private int countOccurrences(char letter, int index) {
 		int counter = 0;
 		// base case
@@ -55,7 +71,33 @@ public class Word {
 		// recursive case
 		return counter + countOccurrences(letter, index + 1);
 	}
-
+	
+	/**
+	 * 
+	 * @param letter
+	 * @param replacement
+	 */
+	public void replaceLastOccurrence(char letter, char replacement) {
+		if (!((replacement >= 'A' && replacement <= 'Z') || (replacement >= 'a' && replacement <= 'z')))
+			throw new IllegalArgumentException("argument passed is not a letter");
+		
+		replaceLastOccurrence(letter, replacement, 0);
+	}
+	
+	/**
+	 * 
+	 * @param letter
+	 * @param replacement
+	 * @param index
+	 */
+	private void replaceLastOccurrence(char letter, char replacement, int index) {
+		if (index == letters.length)
+			return;
+		if (countOccurrences(letter, index) == 1)
+			
+		
+		replaceLastOccurrence(letter, replacement, index + 1);
+	}
 
 	/**
 	 * Validates all characters in the letters array are between A-Z or a-z.
