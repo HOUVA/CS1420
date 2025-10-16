3d8358a99e7efde9de02648456a108efb2a44284
Matthew Suggars <matt.suggars@gmail.com>
2025-10-16 15:14:35 -0600

    assignment 7 updated

--- src/assign07/CandidateSearch.java
+++ src/assign07/CandidateSearch.java
@@ -3,12 +3,29 @@ package assign07;
 public class CandidateSearch {
 	private static int counter;
 	
+	/**
+	 * Returns the value of the static counter variable.
+	 * 
+	 * @return value of counter.
+	 */
 	public static int getCallCount() {
 		return counter;
 	}
 	
+	/**
+	 * 
+	 * @param array
+	 * @param target
+	 */
 	public static void sequentialSearch(Candidate[] array, Candidate target){
-		
+		counter = 0;
+		sequentialSearch(array, target, counter);
 	}
 
+	private static Candidate sequentialSearch(Candidate[] array, Candidate target, int index) {
+		if (index == array.length) return null;
+		else if (array[index].equals(target)) return array[index];
+		
+		return sequentialSearch(array, target, index + 1);
+	}
 }
